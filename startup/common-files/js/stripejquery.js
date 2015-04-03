7f08be24b14bf7e2a8d6fd7ad0b7aee49320b145
Joshua Hancock <jhancock1229@gmail.com>
2015-04-02 20:31:42 -0600

    Working on jQuery for Stripe.

--- startup/common-files/js/stripejquery.js
+++ startup/common-files/js/stripejquery.js
@@ -1,7 +1,7 @@
 var handler = StripeCheckout.configure({
     key: 'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
-    image: '/img/documentation/checkout/marketplace.png',
-    //zipCode: true,
+    image: 'startup/common-files/img/statepaclogo.png',
+    zipCode: true,
     token: function (token) {
         // Use the token to create the charge with a server-side script.
         // You can access the token ID with `token.id`
@@ -12,7 +12,7 @@ $('#customButton').on('click', function (e) {
     // Open Checkout with further options
     handler.open({
         name: 'Statesmen',
-        description: 'A better way for politics',
+        //description: 'A better way for politics',
         amount: 2000,
         zipCode: true
     });
