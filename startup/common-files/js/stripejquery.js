dd885f72b02c3817197ccc74be8c31eda90db4b8
Joshua Hancock <jhancock1229@gmail.com>
2015-04-03 01:02:28 -0600

    Added some features for payment modal. Still not done.

--- startup/common-files/js/stripejquery.js
+++ startup/common-files/js/stripejquery.js
@@ -1,25 +1,68 @@
 var handler = StripeCheckout.configure({
-    key: 'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
-    image: 'startup/common-files/img/statepaclogo.png',
-    zipCode: true,
-    token: function (token) {
-        // Use the token to create the charge with a server-side script.
-        // You can access the token ID with `token.id`
+    key: 'pk_test_bOHcMClPjvhXPod62NShnyh2',
+    //image: '/img/documentation/checkout/marketplace.png',
+    token: function(token) {
+      // Use the token to create the charge with a server-side script.
+      // You can access the token ID with `token.id`
     }
-});
+  });
 
-$('#customButton').on('click', function (e) {
+  $('#ten').on('click', function(e) {
     // Open Checkout with further options
     handler.open({
-        name: 'Statesmen',
-        //description: 'A better way for politics',
-        amount: 2000,
-        zipCode: true
+      name: 'Statesmen',
+      description: 'Donation of $10',
+      amount: 1000
     });
     e.preventDefault();
-});
+  });
+$('#twenty').on('click', function(e) {
+    // Open Checkout with further options
+    handler.open({
+      name: 'Statesmen',
+      description: 'Donation of $20',
+      amount: 2000
+    });
+    e.preventDefault();
+  });
+$('#fifty').on('click', function(e) {
+    // Open Checkout with further options
+    handler.open({
+      name: 'Statesmen',
+      description: 'Donation of $50',
+      amount: 5000
+    });
+    e.preventDefault();
+  });
+$('#oneHundred').on('click', function(e) {
+    // Open Checkout with further options
+    handler.open({
+      name: 'Statesmen',
+      description: 'Donation of $100',
+      amount: 10000
+    });
+    e.preventDefault();
+  });
+$('#oneHundredFifty').on('click', function(e) {
+    // Open Checkout with further options
+    handler.open({
+      name: 'Statesmen',
+      description: 'Donation of $150',
+      amount: 15000
+    });
+    e.preventDefault();
+  });
+$('#max').on('click', function(e) {
+    // Open Checkout with further options
+    handler.open({
+      name: 'Statesmen',
+      description: 'Donation of $199',
+      amount: 19900
+    });
+    e.preventDefault();
+  });
 
-// Close Checkout on page navigation
-$(window).on('popstate', function () {
+  // Close Checkout on page navigation
+  $(window).on('popstate', function() {
     handler.close();
-});
+  });
