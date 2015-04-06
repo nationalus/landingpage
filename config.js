97c39e97ae732661332f9cd464500d9f5e2b397a
Peter Anderson <anderson.peter@byu.edu>
2015-04-06 13:24:50 -0600

    Modifed database url for deployment

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = function() {
     var config = {
         secretKey : process.env.SECRET_KEY || 'sk_test_HXKvNvuINMPeCBcFOORVsP3q',
         port : process.env.PORT || '8443',
-        dbURI : 'mongodb://localhost/landing-test',
+        dbURI : process.env.MONGOLAB_URI || 'mongodb://localhost/landing-test',
         apiKey : process.env.API || 'API Key'
     };
     return config;
