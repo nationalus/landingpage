8a5ddb6c54d0256235ed3fe9bb8f0af0149f9a25
Peter Anderson <anderson.peter@byu.edu>
2015-04-06 12:39:35 -0600

    Reset to http

--- config.js
+++ config.js
@@ -6,8 +6,6 @@ module.exports = function() {
     var config = {
         secretKey : process.env.SECRET_KEY || 'sk_test_HXKvNvuINMPeCBcFOORVsP3q',
         port : process.env.PORT || '8443',
-        key : fs.readFileSync('key.pem').toString(),
-        cert : fs.readFileSync('cert.pem').toString(),
         dbURI : 'mongodb://localhost/landing-test',
         apiKey : process.env.API || 'API Key'
     };
