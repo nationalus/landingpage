8ae65375d838ad46797d3719dd8d7f23f498a452
Peter Anderson <anderson.peter@byu.edu>
2015-04-13 12:08:06 -0600

    Fixed reroute

--- public/startup/common-files/js/index.js
+++ public/startup/common-files/js/index.js
@@ -1,8 +1,7 @@
 //page links
 var link = document.getElementById('payment-page');
     var loc = document.location;
-    link.href = 'http://' + loc.hostname + ':' + loc.port + '/payment.html';
-
+    link.href = loc.protocol + '://' + loc.hostname + ':' + loc.port + '/payment.html';
 
 //modals
 $('#thisButton').click(function () {
