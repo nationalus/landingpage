8ebf2b3b2d4e7db7dad65344a448fdeecb287b5f
Peter Anderson <anderson.peter@byu.edu>
2015-04-14 15:19:22 -0600

    Added compression

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ if (process.env.NODE_ENV === 'production') {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(compression());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
